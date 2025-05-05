6c5e67ca04503bd0da3eb1402b01969c5afe5fa1
vitoriasaturnino <vim.saturnino@gmail.com>
2025-05-05 09:21:07 -0300

    chore: rename SQL constraints to UC_TaskStatement and UC_TaskOrder

--- src/main/resources/db/migration/V3__createTableTask.sql
+++ src/main/resources/db/migration/V3__createTableTask.sql
@@ -7,6 +7,6 @@ CREATE TABLE tasks (
     task_type enum('OPEN_TEXT', 'SINGLE_CHOICE', 'MULTIPLE_CHOICE') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT FK_Course FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE,
-    CONSTRAINT unique_task_statement UNIQUE (course_id, statement),
-    CONSTRAINT unique_task_order UNIQUE (course_id, task_order)
+    CONSTRAINT UC_TaskStatement UNIQUE (course_id, statement),
+    CONSTRAINT UC_TaskOrder UNIQUE (course_id, task_order)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
