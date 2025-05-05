f45c8642044c04fb3bf3e0348e07587645ae4bcc
vitoriasaturnino <vim.saturnino@gmail.com>
2025-05-05 09:21:27 -0300

    test: update publication test to verify successful scenario

--- src/test/java/br/com/alura/AluraFake/course/CoursePublicationIntegrationTest.java
+++ src/test/java/br/com/alura/AluraFake/course/CoursePublicationIntegrationTest.java
@@ -1,17 +1,24 @@
 package br.com.alura.AluraFake.course;
 
 import br.com.alura.AluraFake.task.TaskRepository;
+import br.com.alura.AluraFake.task.open_text.OpenTextTask;
+import br.com.alura.AluraFake.task.single_choice.SingleChoiceTask;
+import br.com.alura.AluraFake.task.multiple_choice.MultipleChoiceTask;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.OK;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class CoursePublicationIntegrationTest {
 
@@ -19,15 +26,26 @@ class CoursePublicationIntegrationTest {
     private TestRestTemplate restTemplate;
 
     @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
     private TaskRepository taskRepository;
 
-    @Sql("/sql/create-test-course.sql")
     @Test
-    void should_NotPublishCourse_When_TasksAreMissing() {
-        // Act
-        var publishResponse = restTemplate.postForEntity("/course/1/publish", null, Void.class);
+    @Sql("/sql/create-test-course.sql")
+    void should_PublishCourse_When_RequirementsAreMet() {
 
-        // Assert
-        assertThat(publishResponse.getStatusCode()).isEqualTo(BAD_REQUEST);
+        var course = courseRepository.findById(1L).orElseThrow();
+        taskRepository.saveAll(List.of(
+                new OpenTextTask(course, "OpenText Task", 1),
+                new SingleChoiceTask(course, "SingleChoice Task", 2),
+                new MultipleChoiceTask(course, "MultipleChoice Task", 3)
+        ));
+
+        var response = restTemplate.postForEntity("/course/1/publish", null, Void.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(OK);
+        var updatedCourse = courseRepository.findById(1L).orElseThrow();
+        assertThat(updatedCourse.getStatus()).isEqualTo(Status.PUBLISHED);
     }
 }
